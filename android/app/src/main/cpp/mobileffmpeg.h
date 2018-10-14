dfddf18b52069775a4985722e6a8ddd19ac61f50
Taner Sener <tanersener@gmail.com>
2018-10-14 12:22:34 +0300

    new major version

--- android/app/src/main/cpp/mobileffmpeg.h
+++ android/app/src/main/cpp/mobileffmpeg.h
@@ -27,7 +27,7 @@
 #include "libavutil/ffversion.h"
 
 /** Library version string */
-#define MOBILE_FFMPEG_VERSION "2.2"
+#define MOBILE_FFMPEG_VERSION "3.0"
 
 /** Defines tag used for Android logging. */
 #define LIB_NAME "mobile-ffmpeg"
