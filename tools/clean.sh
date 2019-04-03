44043868b83fa5ab3f8eddb1cc2055192f0f2f1b
Taner Sener <tanersener@gmail.com>
2019-04-03 19:31:44 +0300

    fix paths in clean script

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 rm -rf ../prebuilt
+rm -rf ../.tmp
 rm -f ../build.log
 
-rm -rf android/build
-rm -rf android/app/build
-rm -rf android/obj
-rm -rf android/libs
-rm -rf test-app/build
+rm -rf ../android/build
+rm -rf ../android/app/build
+rm -rf ../android/obj
+rm -rf ../android/libs
+rm -rf ../android/test-app/build
 
-rm -rf ios/test-app/pods-with-tooltips/Pods
-rm -rf ios/test-app/manual-frameworks/Pods
-rm -rf ios/test-app/manual-frameworks/*.framework
-rm -rf ios/test-app/universal/Pods
-rm -rf ios/test-app/universal/*.framework
-rm -rf ios/test-app/universal/mobile-ffmpeg-universal
+rm -rf ../ios/test-app/pods-with-tooltips/Pods
+rm -rf ../ios/test-app/manual-frameworks/Pods
+rm -rf ../ios/test-app/manual-frameworks/*.framework
+rm -rf ../ios/test-app/universal/Pods
+rm -rf ../ios/test-app/universal/*.framework
+rm -rf ../ios/test-app/universal/mobile-ffmpeg-universal
