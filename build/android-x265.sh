a416890244901aa05c804d1a2d1884894123a80d
Taner Sener <tanersener@gmail.com>
2019-11-02 12:50:40 +0000

    use new x265 version 3.2.1

--- build/android-x265.sh
+++ build/android-x265.sh
@@ -91,6 +91,6 @@ cmake -Wno-dev \
 make -j$(get_cpu_count) || exit 1
 
 # CREATE PACKAGE CONFIG MANUALLY
-create_x265_package_config "3.2"
+create_x265_package_config "3.2.1"
 
 make install || exit 1
