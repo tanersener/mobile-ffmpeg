ef39b78068ddedec7cfdd8f2363c1bc2212325fa
Pete <peter9870@users.noreply.github.com>
2020-08-24 13:48:38 +0100

    Update arch-common.sh (#534)
    * Update arch-common.sh
    
    x265 download on Bitbucket no longer works. Use GitHub link instead

--- build/arch-common.sh
+++ build/arch-common.sh
@@ -263,9 +263,9 @@ download_gpl_library_source() {
     GPL_LIB_ORIG_DIR="x264-cde9a93319bea766a92e306d69059c76de970190"
     ;;
   x265)
-    GPL_LIB_URL="https://bitbucket.org/multicoreware/x265/downloads/x265_3.4.tar.gz"
+    GPL_LIB_URL="https://github.com/videolan/x265/archive/3.4.tar.gz"
     GPL_LIB_FILE="x265_3.4.tar.gz"
-    GPL_LIB_ORIG_DIR="x265_3.4"
+    GPL_LIB_ORIG_DIR="x265-3.4"
     ;;
   xvidcore)
     GPL_LIB_URL="https://downloads.xvid.com/downloads/xvidcore-1.3.7.tar.gz"
